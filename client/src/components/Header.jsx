ca145758efda11fe2f5291fd5882fbe5566ab260
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-10-13 11:46:20 +0300

    extract main components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <>
+            <header className="header">
+                <div className="logo">
+                    <span className="course">React Workshop - October 2025</span>
+                </div>
+            </header>
+        </>
+    )
+}
