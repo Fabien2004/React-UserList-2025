cc2b8353deb02b64fc2a86714438eea8a714a5cc
Fabien2004 <116782951+Fabien2004@users.noreply.github.com>
2025-10-14 13:55:07 +0300

    list user item in user table

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -164,7 +164,7 @@ export default function UserList() {
               </tr>
             </thead>
             <tbody>
-              <UserListItem />
+              {users.map(user => <UserListItem key={user._id} {...user}/>)}
             </tbody>
           </table>
         </div>
